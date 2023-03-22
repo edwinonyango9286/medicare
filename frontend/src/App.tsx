772c969b1cf4352db7e28f2c1eafb4339db6f52c
tonylloyd2 <101291544+tonylloyd2@users.noreply.github.com>
2023-03-22 23:47:36 +0300

    xdgs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import { Route, Routes } from 'react-router-dom'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import Home from './components/Home'
-
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Home from './components/Home';
+import  Chatlist  from './components/chat';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path='/chat' element={<Chatlist />}></Route>
       <Route path="/auth-user-login" element={<Login />} />
       <Route path="/auth-user-register" element={<Register />} />
     </Routes>
