b66c3d6bd69fb506a8e029b6ee6a56cd63044a8f
tonylloyd2 <101291544+tonylloyd2@users.noreply.github.com>
2023-04-13 01:25:26 +0300

    Revert "billing"
    This reverts commit 0ce9cb1dcf6a745f3712da82b27347b2121a6c8f.

--- frontend/src/components/testgpt/Gpt.jsx
+++ frontend/src/components/testgpt/Gpt.jsx
@@ -1,55 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react'
 import axios from 'axios';
 
+const API_URL = 'https://api.openai.com/v1/engine/davinci-codex/completions';
 
-function Gpt () {
-  const API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-  const API_KEY = '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';
+const apiKey = 'YOUR_API_KEY_HERE';
 
-  const generateResponse = async (inputText) => {
-    const response = await axios.post(API_URL, {
-      prompt: inputText,
-      max_tokens: 2048,
-      temperature: 0.7,
-      n: 1,
-      stop: '\n'
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_KEY}`
-      }
-    });
+const generateResponse = async (inputText) => {
+  const response = await axios.post(API_URL, {
+    prompt: inputText,
+    max_tokens: 60,
+    temperature: 0.7,
+    n: 1,
+    stop: '\n'
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    }
+  });
 
-    return response.data.choices[0].text;
-  };
-
-  const [userText, setUserText] = useState('');
-  const [botText, setBotText] = useState('');
-
-  const handleUserTextChange = (event) => {
-    setUserText(event.target.value);
-  };
-
-  const handleUserTextSubmit = async () => {
-    const response = await generateResponse(userText);
-    setBotText(response);
-    setUserText('');
-  };
-
-  return (
-    <div>
-      <div>
-        <label htmlFor="user-text">Your message:</label>
-        <input id="user-text" value={userText} onChange={handleUserTextChange} />
-        <button onClick={handleUserTextSubmit}>Send</button>
-      </div>
-      {botText && (
-        <div>
-          <strong>Bot says:</strong> {botText}
-        </div>
-      )}
-    </div>
-  );
+  return response.data.choices[0].text;
 };
 
-export default Gpt;
+// Example usage:
+generateResponse('Hello, ChatGPT!').then(response => {
+  console.log(response);
+});
+
+const Gpt = () => {
+  return (
+    <div>Gpt</div>
+  )
+}
+
+export default Gpt
